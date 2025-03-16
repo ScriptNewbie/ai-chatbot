15e64e6049592846792d0fb8a4938a9d0b165f18
Marcin Konopka <marcin@konopka.dev>
2025-03-16 12:37:16 +0100

    fix: Missing Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import Chat from "./components/organisms/Chat";
 
 export default function Page() {
   return (
-    <div style={{ width: "100dvw", height: "100dvh" }}>
-      <Chat />
-    </div>
+    <Suspense>
+      <div style={{ width: "100dvw", height: "100dvh" }}>
+        <Chat />
+      </div>
+    </Suspense>
   );
 }
