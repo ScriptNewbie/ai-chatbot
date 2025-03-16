78cab4f5a2d05d107a9674335b5171aa7f01f2b3
Marcin Konopka <marcin@konopka.dev>
2025-03-16 12:47:12 +0100

    chore: Remove unused imports

--- app/components/organisms/Chat.tsx
+++ app/components/organisms/Chat.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
 import { ChatContainer } from "../atoms/ChatContainer";
 import { ChatEntry } from "../atoms/ChatMessage";
 import { ChatActions } from "../molecules/ChatActions";
